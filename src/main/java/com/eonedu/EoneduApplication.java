aebbd67a2acf28640334a192ae9509b862a94b2c
seonghun <kimsh8580@gmail.com>
2024-08-01 22:00:05 +0900

    first commit

---
+++ src/main/java/com/eonedu/EoneduApplication.java
@@ -0,0 +1,13 @@
+package com.eonedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EoneduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EoneduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eonedu/EoneduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eonedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EoneduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

fde2964f0c6985789fd9b43cb750454ce8b54e03
seonghun <kimsh8580@gmail.com>
2024-08-03 16:54:03 +0900

    [FEAT]: 서버 DB 연결 테스트

--- src/main/java/com/eonedu/domain/branch/domain/Branch.java
+++ src/main/java/com/eonedu/domain/branch/domain/Branch.java
@@ -1,11 +1,9 @@
 package com.eonedu.domain.branch.domain;
 
 import com.eonedu.domain.common.model.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
 public class Branch extends BaseEntity{
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "branch_id")

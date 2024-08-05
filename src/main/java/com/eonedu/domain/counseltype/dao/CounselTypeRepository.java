7332283c9dac210d71b83599b5100601c9d83994
seonghun <kimsh8580@gmail.com>
2024-08-05 16:19:08 +0900

    feat: create CounselType domain
    create CounselType entity, dao, application (create & findAll)

---
+++ src/main/java/com/eonedu/domain/counseltype/dao/CounselTypeRepository.java
@@ -0,0 +1,7 @@
+package com.eonedu.domain.counseltype.dao;
+
+import com.eonedu.domain.counseltype.domain.CounselType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CounselTypeRepository extends JpaRepository<CounselType, Long> {
+}
---
+++ src/main/java/com/eonedu/domain/counseltype/domain/CounselType.java
@@ -0,0 +1,24 @@
+package com.eonedu.domain.counseltype.domain;
+
+import com.eonedu.domain.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class CounselType extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "counsel_type_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public static CounselType from(String name) {
+        CounselType counselType = new CounselType();
+        counselType.setName(name);
+
+        return counselType;
+    }
+}

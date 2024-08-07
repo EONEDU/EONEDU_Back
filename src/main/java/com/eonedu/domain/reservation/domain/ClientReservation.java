ee825845c0c57e2d5b356fcedc612553d3021cd8
seonghun <kimsh8580@gmail.com>
2024-08-07 13:16:31 +0900

    refactor: add protected basic constructor
    add protected constructor for JPA
    add comment

--- src/main/java/com/eonedu/domain/reservation/domain/ClientReservation.java
+++ src/main/java/com/eonedu/domain/reservation/domain/ClientReservation.java
@@ -7,12 +7,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
-@NoArgsConstructor
 @Getter
 public class ClientReservation extends Reservation{
     @Column(nullable = false)
@@ -22,13 +20,17 @@ public class ClientReservation extends Reservation{
     private String clientPhone;
 
     @Column(unique = true, nullable = false)
-    private final String reservationRandomId = RandomIdMaker.makeTwelveRandomId();
+    private String reservationRandomId;
 
     @Column(nullable = false)
     private ReservationState state;
 
+    //JPA용 기본 생성자
+    protected ClientReservation() {}
+
+    // 매게변수가 많아 Builder를 사용하여 객체를 생성
     @Builder
-    public ClientReservation(Long id, CounselType counselType, Branch branch, LocalDate date, ReservationTime time,  String clientName, String clientPhone) {
+    private ClientReservation(Long id, CounselType counselType, Branch branch, LocalDate date, ReservationTime time,  String clientName, String clientPhone) {
         this.id = id;
         this.counselType = counselType;
         this.branch = branch;
@@ -36,6 +38,7 @@ public class ClientReservation extends Reservation{
         this.time = time;
         this.clientName = clientName;
         this.clientPhone = clientPhone;
-        this.state = ReservationState.WAITING;
+        this.state = ReservationState.WAITING; // 기본 상태는 대기로 설정
+        this.reservationRandomId = RandomIdMaker.makeTwelveRandomId(); // 예약 고유번호 생성
     }
 }

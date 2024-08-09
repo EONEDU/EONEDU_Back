2806ead1176350937ff1f5549a5031a2f1009037
seonghun <kimsh8580@gmail.com>
2024-08-09 16:26:16 +0900

    feat: add validate message
    add message in validation annotation

--- src/main/java/com/eonedu/domain/reservation/dto/request/ClientReservationCreateRequest.java
+++ src/main/java/com/eonedu/domain/reservation/dto/request/ClientReservationCreateRequest.java
@@ -6,10 +6,10 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record ClientReservationCreateRequest (
-        @NotNull Long branchId,
-        @NotNull Long counselTypeId,
-        @NotNull LocalDate date,
-        @NotBlank String time,
-        @NotBlank String clientName,
-        @NotBlank String clientPhone
+        @NotNull(message = "지점은 비워둘 수 없습니다") Long branchId,
+        @NotNull(message = "상담 종류는 비워둘 수 없습니다") Long counselTypeId,
+        @NotNull(message = "날짜는 비워둘 수 없습니다") LocalDate date,
+        @NotBlank(message = "시간은 비워둘 수 없습니다") String time,
+        @NotBlank(message = "고객 이름은 비워둘 수 없습니다") String clientName,
+        @NotBlank(message = "고객 전화번호는 비워둘 수 없습니다") String clientPhone
 ) {}
